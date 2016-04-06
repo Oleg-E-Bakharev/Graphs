18355ff5c130f1d57b967f0d84f79add0fb715d1
Oleg Bakharev <oleg_bakharev@mail.ru>
2016-04-06 18:35:55 +0300

    Initial Commit

---
+++ Graphs/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Graphs
+//
+//  Created by Oleg Bakharev on 06/04/16.
+//  Copyright © 2016 Oleg Bakharev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
